996cfc9ea8242f8d7a6b5497e54af432f30cad3d
Charumathi Hariharan <86341150+charuharan@users.noreply.github.com>
2021-08-14 16:04:03 +0530

    Add files via upload

---
+++ timeout.js
@@ -0,0 +1,18 @@
+//zero delay
+/*setTimeout(() => {
+    console.log('after ');
+  }, 0)
+  
+  console.log(' before ');*/
+
+setTimeout(() => {
+    console.log("runs after 2 seconds");
+  }, 2000)
+  
+  setTimeout(() => {
+    console.log("runs after 50 milliseconds");
+  }, 50)
+
+  
+  
+  
